500babf164ae112eecc5f0a805262a1d938b12d2
akber498 <akber498@gmail.com>
2020-03-10 22:30:16 -0400

    rephrased the lines and added

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ echo "this is from github"
 NUM=$(($NUMONE + $NUMTWO))
 echo "$NUM"
 echo "This completes test"
-echo "git hub hook test successfully tested"
+echo "git hub hook test successfully tested and removed slash from the project URL which is error "
